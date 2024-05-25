f13c666d083cdf143f11c591c41cc91326e27b2e
John Ehssan <johnehssan@outlook.com>
2024-05-25 09:22:25 +0100

    initial-setup-complete

---
+++ src/app/thought.model.ts
@@ -0,0 +1,6 @@
+export interface Thought {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: Date;
+}
--- src/app/thought.service.ts
+++ src/app/thought.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Thought } from './thought.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThoughtService {
+  private apiUrl = 'http://localhost:5000/api/thoughts';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getThoughts(): Observable<Thought[]> {
+    return this.http.get<Thought[]>(this.apiUrl);
+  }
+
+  getThoughtById(id: number): Observable<Thought> {
+    return this.http.get<Thought>(`${this.apiUrl}/${id}`);
+  }
+
+  createThought(thought: Thought): Observable<Thought> {
+    return this.http.post<Thought>(this.apiUrl, thought);
+  }
+
+  updateThought(thought: Thought): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${thought.id}`, thought);
+  }
+
+  deleteThought(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
