f3d282fa9f0ce5faa444634e2b4277af85b2c066
johnehssan <johnehssan@outlook.com>
2025-01-26 11:32:10 +0000

    TCD19 Implement Window Control Functions
    Implemented functions for minimizing, maximizing/restoring, and closing the window using Electron's `remote` module.

--- src/app/custom-title-bar/custom-title-bar.component.ts
+++ src/app/custom-title-bar/custom-title-bar.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare global {
+  interface Window {
+    electronAPI: {
+      minimizeWindow: () => void;
+      maximizeWindow: () => void;
+      unmaximizeWindow: () => void;
+      closeWindow: () => void;
+      isMaximized: () => Promise<boolean>;
+    };
+  }
+}
 
 @Component({
   selector: 'app-custom-title-bar',
   templateUrl: './custom-title-bar.component.html',
-  styleUrl: './custom-title-bar.component.css'
+  styleUrls: ['./custom-title-bar.component.css']
 })
-export class CustomTitleBarComponent {
+export class CustomTitleBarComponent implements OnInit {
+  isMaximized: boolean = false;
+  windowTitle: string = 'My App';
 
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('Window electronAPI:', window.electronAPI);
+
+    if (window.electronAPI) {
+      window.electronAPI.isMaximized().then((result) => {
+        this.isMaximized = result;
+      });
+    }
+  }
+
+  minimize() {
+    if (window.electronAPI) {
+      window.electronAPI.minimizeWindow();
+    }
+  }
+
+  toggleMaximizeRestore() {
+    if (window.electronAPI) {
+      window.electronAPI.isMaximized().then((result) => {
+        this.isMaximized = result;
+        if (this.isMaximized) {
+          window.electronAPI.unmaximizeWindow();
+        } else {
+          window.electronAPI.maximizeWindow();
+        }
+      });
+    }
+  }
+
+  close() {
+    if (window.electronAPI) {
+      window.electronAPI.closeWindow();
+    }
+  }
+
+  toggleNavigation() {
+    console.log('Navigation toggled');
+  }
 }
